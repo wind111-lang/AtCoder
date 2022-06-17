1f263e968441e39a49646b953566515b9949e107
Shota Tsutsui <k20076kk@aitech.ac.jp>
2022-06-17 21:31:34 +0900

    ABC250 B AC

--- ABC250/B.go
+++ ABC250/B.go
@@ -2,27 +2,32 @@ package main
 
 import (
 	"fmt"
-	"math"
+	_"math"
 )
 
 func main() {
 	var n, a, b int
-
+	var x [][]string
+	//var val string
+	cnt := 0
 	fmt.Scanf("%d %d %d", &n, &a, &b)
 
-	for i := 1; i <= n*a; i++ {
-		if (math.Abs(float64(i-a)))+(math.Abs(float64(i-b))) == 1 {
-			fmt.Print("#")
-		} else {
-			fmt.Print(".")
-		}
-		for j := 1; j < n*b; i++ {
-			if (math.Abs(float64(j-a)))+(math.Abs(float64(j-b))) == 1 {
-				fmt.Print(".")
+	for i := 0; i < n*a; i++ {
+		for j := 0; j<n*b; j++{
+			if ((i/a)+(j/b))%2 == 0 {
+				x = append(x, []string{"."})
 			} else {
-				fmt.Print("#")
+				x = append(x, []string{"#"})
 			}
-		}
-		fmt.Println()
+		} 
 	}
+	for val := range x {
+		if cnt==(n*b) {
+			fmt.Println()
+			cnt = 0
+		}
+		fmt.Print(x[val][0])
+		cnt++
+	}
+	fmt.Println()
 }
