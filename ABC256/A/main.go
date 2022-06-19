90d9ebe91d85274dd3a773dec84cee2aa94277b5
Shota Tsutsui <k20076kk@aitech.ac.jp>
2022-06-19 14:26:35 +0900

    ABC256

--- ABC256/A/main.go
+++ ABC256/A/main.go
@@ -2,12 +2,35 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
-	var n int
+	var n float64
 
 	fmt.Scan(&n)
+	pow := math.Pow(2, n)
+	fmt.Println(int(pow))
 
 	//fmt.Println(n)
 }
+
+//別解
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	var n int
+// 	pow := 1
+
+// 	fmt.Scan(&n)
+// 	for i := 0; i < n; i++ {
+// 		pow = pow << 1
+// 	}
+
+// 	fmt.Println(pow)
+// }
--- ABC256/B/main.go
+++ ABC256/B/main.go
@@ -5,13 +5,23 @@ import (
 )
 
 func main() {
-	var n int
+	var n,x int
+	//var a,b,c,d int
+
+	p := 0
 
 	fmt.Scan(&n)
 
-	//a := make([]int, 0, n)
-	//for i := 0; i < n; i++ {
+	slice := make([]int, 0, n)
 
-	//}
-	//fmt.Println(n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&x)
+		slice = append(slice, x)
+	}
+
+	for mass := range slice {
+		fmt.Println(mass)
+	}
+
+	fmt.Println(p)
 }
